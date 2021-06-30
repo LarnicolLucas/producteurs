d3d489456cc9e78292b7f030a50b5def1a177dd0
Lucas <lucas.larnicol@gmail.com>
2021-06-30 14:30:20 +0200

    create Donut graph

---
+++ components/donut/createParams.js
@@ -0,0 +1,39 @@
+export default function params(params, ctx){
+
+    const data = {
+        labels: params.legends,
+        datasets: [{
+            label: params.label,
+            data: params.data,
+            backgroundColor: params.color,
+            hoverOffset: 25
+        }]
+    };
+    let delayed;
+    return {
+        type: 'doughnut',
+        data: data,
+        options: {
+            animation: {
+              onComplete: () => {
+                delayed = true;
+              },
+              delay: (context) => {
+                let delay = 0;
+                if (context.type === 'data' && context.mode === 'default' && !delayed) {
+                  delay = context.dataIndex * 300 + context.datasetIndex * 100;
+                }
+                return delay;
+              },
+            },
+            scales: {
+              x: {
+                stacked: true,
+              },
+              y: {
+                stacked: true
+              }
+            }
+          }
+    }
+}
