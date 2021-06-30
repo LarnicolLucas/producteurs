a83d1541bd4c2822151691c7bc3c4b57c20ab8b5
Lucas <lucas.larnicol@gmail.com>
2021-06-30 21:44:20 +0200

    add description

--- components/energies/energieSlect/energieSelect.js
+++ components/energies/energieSlect/energieSelect.js
@@ -3,12 +3,12 @@ import styles from "./energieSelect.module.sass"
 
 export default function EnergieSelect(props){
     const list = [
-        {id: 0, src: "/images/icons_producteurs/eolienne.svg", title: "Eolien"},
-        {id: 1, src: "/images/icons_producteurs/hydrolique.svg", title: "Hydrolique"},
-        {id: 2, src: "/images/icons_producteurs/solaire.svg", title: "Solaire"},
-        {id: 3, src: "/images/icons_producteurs/thermique.svg", title: "Thermique"},
-        {id: 4, src: "/images/icons_producteurs/bioenergie.svg", title: "Bioénergie"}
-    ].map(el => <Card key ={el.id} src={el.src} title={el.title}/>)
+        {id: 0, src: "/images/icons_producteurs/eolienne.svg", title: "Eolien", description: "Client diposant d'un moyen de production de type éolien."},
+        {id: 1, src: "/images/icons_producteurs/hydrolique.svg", title: "Hydrolique", description: "Point de production au moyen d'un générateur hydrolique"},
+        {id: 2, src: "/images/icons_producteurs/solaire.svg", title: "Solaire", description: "Client possédant des panneaux solaires raccordé au réseau"},
+        {id: 3, src: "/images/icons_producteurs/thermique.svg", title: "Thermique", description: "Centrale de production thermiques vapeur, combustion etc..."},
+        {id: 4, src: "/images/icons_producteurs/bioenergie.svg", title: "Bioénergie", description: "Client utilisant des centrales de production bioénergetique"}
+    ].map(el => <Card key ={el.id} src={el.src} title={el.title} description={el.description} color={props.color}/>)
     return <>
         <aside className={styles.container}>
             {list}
