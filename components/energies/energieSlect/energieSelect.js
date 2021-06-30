aebe83ddc920bd23d793bdf16ef71d5f76631701
Lucas <lucas.larnicol@gmail.com>
2021-06-30 14:16:48 +0200

    fix orthographe and add bioenergies

--- components/energies/energieSlect/energieSelect.js
+++ components/energies/energieSlect/energieSelect.js
@@ -4,9 +4,10 @@ import styles from "./energieSelect.module.sass"
 export default function EnergieSelect(props){
     const list = [
         {id: 0, src: "/images/icons_producteurs/eolienne.svg", title: "Eolien"},
-        {id: 1, src: "/images/icons_producteurs/hydrolique.svg", title: "Hyrolique"},
+        {id: 1, src: "/images/icons_producteurs/hydrolique.svg", title: "Hydrolique"},
         {id: 2, src: "/images/icons_producteurs/solaire.svg", title: "Solaire"},
-        {id: 3, src: "/images/icons_producteurs/thermique.svg", title: "Thermique"}
+        {id: 3, src: "/images/icons_producteurs/thermique.svg", title: "Thermique"},
+        {id: 4, src: "/images/icons_producteurs/bio.svg", title: "Bioénergie"}
     ].map(el => <Card key ={el.id} src={el.src} title={el.title}/>)
     return <>
         <aside className={styles.container}>
