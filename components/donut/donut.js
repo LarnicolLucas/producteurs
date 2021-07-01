2d7f129f54cd388190c5f9f023cfea1e5c66c6f5
Lucas <lucas.larnicol@gmail.com>
2021-07-01 21:48:42 +0200

    clear canvas

--- components/donut/donut.js
+++ components/donut/donut.js
@@ -4,24 +4,16 @@ import Params from './createParams'
 
 export default function Graph(props){
 
-    /*<Graph datas={{
-        label : 'Graph Test',
-        data: [50, 10, 50, 90],
-        legends: ["Sisi","Test", "Kariis", "test"],
-        color: color
-        }}
-        />
-    */
-
     const canvas = useRef();
+
 
     useEffect(()=> {
 
         const params = Params(props.datas, canvas.current);
         const graph = new Chart(canvas.current.getContext('2d'), params);
 
-        //return ()=> delete graph
-    }, [])
+        return ()=> graph.destroy()
+    })
     
     return <>
         <canvas ref={canvas} width="400" height="400"></canvas>
