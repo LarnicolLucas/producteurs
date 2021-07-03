a9b8af627296f12ea497e7f150666646989ef59e
Lucas <lucas.larnicol@gmail.com>
2021-07-03 08:50:51 +0200

    move list of item

--- components/energies/energieSlect/energieSelect.js
+++ components/energies/energieSlect/energieSelect.js
@@ -1,17 +1,15 @@
+import {Link} from 'next/link'
 import Card from "../card/card"
 import styles from "./energieSelect.module.sass"
+import List from "./list"
 
 export default function EnergieSelect(props){
-    const list = [
-        {id: 0, src: "/images/icons_producteurs/eolienne.svg", title: "Eolien", description: "Client diposant d'un moyen de production de type éolien."},
-        {id: 1, src: "/images/icons_producteurs/hydrolique.svg", title: "Hydrolique", description: "Point de production au moyen d'un générateur hydrolique"},
-        {id: 2, src: "/images/icons_producteurs/solaire.svg", title: "Solaire", description: "Client possédant des panneaux solaires raccordé au réseau"},
-        {id: 3, src: "/images/icons_producteurs/thermique.svg", title: "Thermique", description: "Centrale de production thermiques vapeur, combustion etc..."},
-        {id: 4, src: "/images/icons_producteurs/bioenergie.svg", title: "Bioénergie", description: "Client utilisant des centrales de production bioénergetique"}
-    ].map(el => <Card key ={el.id} src={el.src} title={el.title} description={el.description} color={props.color}/>)
+    const listView = List.map(el =>
+            <Card key={el.list} src={el.src} title={el.title} description={el.description} color={props.color} link={el.link}/>
+    )
     return <>
         <aside className={styles.container}>
-            {list}
+            {listView}
         </aside>
     </>
 }
---
+++ components/energies/energieSlect/list.js
@@ -0,0 +1,39 @@
+const List = [
+    {
+        id: 0, 
+        src: "/images/icons_producteurs/eolienne.svg", 
+        title: "Eolien", 
+        description: "Client diposant d'un moyen de production de type éolien.",
+        link: "F4"
+    },
+    {
+        id: 1, 
+        src: "/images/icons_producteurs/hydrolique.svg", 
+        title: "Hydrolique", 
+        description: "Point de production au moyen d'un générateur hydrolique",
+        link: "F3"
+    },
+    {
+        id: 2, 
+        src: "/images/icons_producteurs/solaire.svg", 
+        title: "Solaire", 
+        description: "Client possédant des panneaux solaires raccordé au réseau",
+        link: "F5"
+    },
+    {
+        id: 3, 
+        src: "/images/icons_producteurs/thermique.svg", 
+        title: "Thermique", 
+        description: "Centrale de production thermiques vapeur, combustion etc...",
+        link: "F1"
+    },
+    {
+        id: 4, 
+        src: "/images/icons_producteurs/bioenergie.svg", 
+        title: "Bioénergie", 
+        description: "Client utilisant des centrales de production bioénergetique",
+        link: "F2"
+    }
+];
+
+export default List
