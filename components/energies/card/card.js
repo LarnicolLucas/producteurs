2f9ce6e3691669d3ac6725758ce0d85250a75f02
Lucas <lucas.larnicol@gmail.com>
2021-07-03 08:50:37 +0200

    add link to each cards

--- components/energies/card/card.js
+++ components/energies/card/card.js
@@ -2,14 +2,16 @@ import styles from './card.module.sass'
 export default function Card(props){
     return <>
         <section className={styles.container}>
-            <figure className={styles.figure}>
-                <img className={styles.img} src={props.src} />
-            </figure>
-            <figcaption className={styles.title}>
-                <h4 className={styles.titleDescription}>{props.title}</h4>
-                <p style={{color: props.color[1]}}>{props.description}</p>
-                
-            </figcaption>
+            <a href={`/energie/${props.link}`}>
+                <figure className={styles.figure}>
+                    <img className={styles.img} src={props.src} />
+                </figure>
+                <figcaption className={styles.title}>
+                    <h4 className={styles.titleDescription}>{props.title}</h4>
+                    <p style={{color: props.color[1]}}>{props.description}</p>
+                    
+                </figcaption>
+            </a>
         </section>
     </>
 }
