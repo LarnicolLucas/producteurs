87bc335de35b285b030bd9a8f49910e6ebd68d0c
Lucas <lucas.larnicol@gmail.com>
2021-07-03 22:19:34 +0200

    add list of producteurs

---
+++ components/listProducteur/listProducteur.js
@@ -0,0 +1,10 @@
+const list = {
+    "F0" : "toute fillières de productions",
+    "F1" : "thermique non renouvelable",
+    "F2" : "bioénergie",
+    "F3" : "hydrolique",
+    "F4" : "eolien",
+    "F5" : "solaire",
+    "F6": "autres fillières de productions"
+}
+export default list
