d277f0e71eaa329c6e733cf1a77231b4f5e74c39
Lucas <lucas.larnicol@gmail.com>
2021-07-04 11:03:05 +0200

    add id on card list

--- components/energies/energieSlect/energieSelect.js
+++ components/energies/energieSlect/energieSelect.js
@@ -1,11 +1,11 @@
-import {Link} from 'next/link'
+
 import Card from "../card/card"
 import styles from "./energieSelect.module.sass"
 import List from "./list"
 
 export default function EnergieSelect(props){
     const listView = List.map(el =>
-            <Card key={el.list} src={el.src} title={el.title} description={el.description} color={props.color} link={el.link}/>
+            <Card key={el.id} src={el.src} title={el.title} description={el.description} color={props.color} link={el.link}/>
     )
     return <>
         <aside className={styles.container}>
