0a78f0eb4af1b620d115cf2777cf28050bae05d9
Lucas <lucas.larnicol@gmail.com>
2021-07-06 14:04:41 +0200

    add home nav

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -21,6 +21,10 @@ export default function Nav(props){
     return <>
         <aside style={style} className={styles.container}>
 
+            <header className={styles.home} >
+                <Pins src={"/images/home/home.svg"} title="Accueil" color={palette} link="../"/>
+            </header>
+
             {elems}
 
         </aside>
