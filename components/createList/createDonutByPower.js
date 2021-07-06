60a313f758cbc24d8c884bee848fc73988628922
Lucas <lucas.larnicol@gmail.com>
2021-07-06 14:59:21 +0200

    remove all type ofpwoer

--- components/createList/createDonutByPower.js
+++ components/createList/createDonutByPower.js
@@ -1,8 +1,9 @@
 export default function CreateDonutByPower(data){
     const addListItem = data.map(el => Object.assign({}, el, {listItem: el.fields.plage_de_puissance_injection.split(" : ")[0].slice(1)}));
     const orderedArray = addListItem.sort((itemA, itemB) => itemA.listItem > itemB.listItem ? -1 : 1);
+    const dataCutF0 = orderedArray.filter(el => el.fields.plage_de_puissance_injection != "P0 : Total toutes puissances");
     return {
-        data: orderedArray.map(el => el.fields.nombre_points_injection).reverse(),
-        legend: orderedArray.map(el => el.fields.plage_de_puissance_injection).reverse()
+        data: dataCutF0.map(el => el.fields.nombre_points_injection).reverse(),
+        legend: dataCutF0.map(el => el.fields.plage_de_puissance_injection).reverse()
     }
 }
