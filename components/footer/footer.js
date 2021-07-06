baf502395b1f99b6f9132ff46013147a0b3af7b2
Lucas <lucas.larnicol@gmail.com>
2021-07-06 10:37:28 +0200

    add background footer

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,10 +1,15 @@
 import styles from './footer.module.sass'
 import SocialMedia from '../socialMedia/socialMedia'
+import palette from '../palette/palette'
 
 export default function Footer(props){
     
+    const style={
+        background: palette[0]
+    };
+
     return <>
-        <footer className={styles.container}>
+        <footer style={style} className={styles.container}>
             <SocialMedia />
         </footer>
     </>
