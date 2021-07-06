cf19e8239d0aacf75c20fb92ef75f2a5fbd6e448
Lucas <lucas.larnicol@gmail.com>
2021-07-06 10:17:54 +0200

    change link of svg card img

--- components/energies/energieSlect/list.js
+++ components/energies/energieSlect/list.js
@@ -1,35 +1,35 @@
 const List = [
     {
         id: 0, 
-        src: "/images/icons_producteurs/eolienne.svg", 
+        src: "/images/icons_producteurs/aside/eolienne.svg", 
         title: "Eolien", 
         description: "Client diposant d'un moyen de production de type éolien.",
         link: "F4"
     },
     {
         id: 1, 
-        src: "/images/icons_producteurs/hydrolique.svg", 
+        src: "/images/icons_producteurs/aside/hydrolique.svg", 
         title: "Hydrolique", 
         description: "Point de production au moyen d'un générateur hydrolique",
         link: "F3"
     },
     {
         id: 2, 
-        src: "/images/icons_producteurs/solaire.svg", 
+        src: "/images/icons_producteurs/aside/solaire.svg", 
         title: "Solaire", 
         description: "Client possédant des panneaux solaires raccordé au réseau",
         link: "F5"
     },
     {
         id: 3, 
-        src: "/images/icons_producteurs/thermique.svg", 
+        src: "/images/icons_producteurs/aside/thermique.svg", 
         title: "Thermique", 
         description: "Centrale de production thermiques vapeur, combustion etc...",
         link: "F1"
     },
     {
         id: 4, 
-        src: "/images/icons_producteurs/bioenergie.svg", 
+        src: "/images/icons_producteurs/aside/bioenergie.svg", 
         title: "Bioénergie", 
         description: "Client utilisant des centrales de production bioénergetique",
         link: "F2"
