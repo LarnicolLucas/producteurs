01096a03c83675ef964ed7099f522ea179344e8c
Lucas <lucas.larnicol@gmail.com>
2021-07-06 14:58:12 +0200

    create link for icon

---
+++ components/iconLink/iconLink.js
@@ -0,0 +1,9 @@
+const icons = {
+    "F1" : "/images/icons_producteurs/aside/thermique.svg",
+    "F2" : "/images/icons_producteurs/aside/bioenergie.svg",
+    "F3" : "/images/icons_producteurs/aside/hydrolique.svg",
+    "F4" : "/images/icons_producteurs/aside/eolienne.svg",
+    "F5" : "/images/icons_producteurs/aside/solaire.svg",
+};
+
+export default icons
