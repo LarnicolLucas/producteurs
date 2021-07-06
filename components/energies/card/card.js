cd063577dbf6e0acb31cf6b809a899d0c2aad512
Lucas <lucas.larnicol@gmail.com>
2021-07-06 10:15:44 +0200

    add style to cards

--- components/energies/card/card.js
+++ components/energies/card/card.js
@@ -1,14 +1,14 @@
 import styles from './card.module.sass'
 export default function Card(props){
     return <>
-        <section className={styles.container}>
-            <a href={`/energie/${props.link}`}>
+        <section>
+            <a className={styles.container} href={`/energie/${props.link}`}>
                 <figure className={styles.figure}>
                     <img className={styles.img} src={props.src} />
                 </figure>
                 <figcaption className={styles.title}>
                     <h4 className={styles.titleDescription}>{props.title}</h4>
-                    <p style={{color: props.color[1]}}>{props.description}</p>
+                    <p className={styles.p} style={{color: props.color[1]}}>{props.description}</p>
                     
                 </figcaption>
             </a>
