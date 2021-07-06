3044b7bed5a7d2d75adfef54a0e11d7b299c9f3c
Lucas <lucas.larnicol@gmail.com>
2021-07-06 10:11:59 +0200

    add link to social medias

--- components/socialMedia/socialMedia.js
+++ components/socialMedia/socialMedia.js
@@ -1,15 +1,16 @@
 import styles from './socialMedia.module.sass'
+import Link from 'next/link'
 
 export default function socialMedia(props){
 
     const list = [
-        {id: 0, src: "/images/socialMedia/github.svg"},
-        {id: 1, src: "/images/socialMedia/linkdin.svg"},
-        {id: 2, src: "/images/socialMedia/openData.svg"},
-        {id: 3, src: "/images/socialMedia/web.svg"}
-    ].map(el => <figure className={styles.figure} key={el.id}>
+        {id: 0, src: "/images/socialMedia/github.svg", link: "https://github.com/LarnicolLucas/producteurs"},
+        {id: 1, src: "/images/socialMedia/linkdin.svg", link: "https://www.linkedin.com/in/lucas-larnicol-52a995201/"},
+        {id: 2, src: "/images/socialMedia/openData.svg", link: "https://data.enedis.fr/pages/accueil/?id=init"},
+        {id: 3, src: "/images/socialMedia/web.svg", link: "https://www.llarnicol.com/"}
+    ].map(el => <Link key={el.id} href={el.link} passHref><figure className={styles.figure}>
         <img className={styles.img} src={el.src} />
-    </figure>)
+    </figure></Link>)
     
     return <>
 
