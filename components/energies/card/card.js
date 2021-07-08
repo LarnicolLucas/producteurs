43c3628bd10a48fabbc516a2b4c9ec0cbd0754ff
Lucas <lucas.larnicol@gmail.com>
2021-07-08 14:41:35 +0200

    add Image to card

--- components/energies/card/card.js
+++ components/energies/card/card.js
@@ -1,13 +1,17 @@
 import styles from './card.module.sass'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Card(props){
+    const imgSrc = props.src == undefined ? "/" : props.src;
     return <>
         <Link href={`/energie/${props.link}`} passHref>
             <section className={styles.container}>
             
                 <figure className={styles.figure}>
-                    <img className={styles.img} src={props.src} />
+                    <div className={styles.img}>
+                        <Image src={imgSrc} alt='Icon of Energie' layout="fill" />
+                    </div>
                 </figure>
                 <figcaption className={styles.title}>
                     <h4 className={styles.titleDescription}>{props.title}</h4>
