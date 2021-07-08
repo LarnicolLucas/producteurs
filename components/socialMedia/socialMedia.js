7e9a140ce1cbedc41e9b34073ee2ca738e7bc311
Lucas <lucas.larnicol@gmail.com>
2021-07-08 15:38:53 +0200

    change domain img links

--- components/socialMedia/socialMedia.js
+++ components/socialMedia/socialMedia.js
@@ -5,10 +5,10 @@ import Image from 'next/image'
 export default function socialMedia(props){
 
     const list = [
-        {id: 0, src: "/images/socialMedia/github.svg", link: "https://github.com/LarnicolLucas/producteurs"},
-        {id: 1, src: "/images/socialMedia/linkdin.svg", link: "https://www.linkedin.com/in/lucas-larnicol-52a995201/"},
-        {id: 2, src: "/images/socialMedia/openData.svg", link: "https://data.enedis.fr/pages/accueil/?id=init"},
-        {id: 3, src: "/images/socialMedia/web.svg", link: "https://www.llarnicol.com/"}
+        {id: 0, src: "/socialMedia/github.svg", link: "https://github.com/LarnicolLucas/producteurs"},
+        {id: 1, src: "/socialMedia/github.svg", link: "https://www.linkedin.com/in/lucas-larnicol-52a995201/"},
+        {id: 2, src: "/socialMedia/github.svg", link: "https://data.enedis.fr/pages/accueil/?id=init"},
+        {id: 3, src: "/socialMedia/github.svg", link: "https://www.llarnicol.com/"}
     ].map(el => <Link key={el.id} href={el.link} passHref><figure className={styles.figure}>
         <article className={styles.img}>
             <Image src={el.src} alt='Social Media img SVG' layout="fill" />
--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   reactStrictMode: true,
+  images:{
+    domains:[
+      'https://producteur.s3.eu-west-3.amazonaws.com'
+    ]
+  }
 }
