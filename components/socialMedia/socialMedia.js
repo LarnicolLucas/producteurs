f0db39384e067f8852d79e0f541b738051cb5db5
Lucas <lucas.larnicol@gmail.com>
2021-07-08 15:07:39 +0200

    add social media IMAGE

--- components/socialMedia/socialMedia.js
+++ components/socialMedia/socialMedia.js
@@ -1,5 +1,6 @@
 import styles from './socialMedia.module.sass'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function socialMedia(props){
 
@@ -9,7 +10,9 @@ export default function socialMedia(props){
         {id: 2, src: "/images/socialMedia/openData.svg", link: "https://data.enedis.fr/pages/accueil/?id=init"},
         {id: 3, src: "/images/socialMedia/web.svg", link: "https://www.llarnicol.com/"}
     ].map(el => <Link key={el.id} href={el.link} passHref><figure className={styles.figure}>
-        <img className={styles.img} src={el.src} />
+        <article className={styles.img}>
+            <Image src={el.src} alt='Social Media img SVG' layout="fill" />
+        </article>
     </figure></Link>)
     
     return <>
