b440ad7d481e602327d57a659c07a27a3b738791
Lucas <lucas.larnicol@gmail.com>
2021-07-08 14:26:00 +0200

    add Image to block

--- components/blockInfo/blockInfo.js
+++ components/blockInfo/blockInfo.js
@@ -1,20 +1,23 @@
 import styles from './blockInfo.module.sass'
+import Image from 'next/image'
 
 export default function blockInfo(props){
 
     const style= {
         background: props.data.color
-    }
+    };
 
     const secondaryTextStyle= {
         color: props.data.txt2Color
-    }
+    };
+
+    const imgSrc = props.data.src == undefined ? '/' : props.data.src;
     
     return <>
         <section style={style} className={styles.container}>
 
             <figure className={styles.figure}>
-                <img className={styles.img} src={props.data.src} />
+                <Image className={styles.img} src={imgSrc} alt='Header Icon' layout="fill" />
             </figure>
             <h4 className={styles.titre}>{props.data.titre}</h4>
             <p style={secondaryTextStyle} className={styles.p}>
