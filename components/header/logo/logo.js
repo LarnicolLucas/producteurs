95c013df6f28f12df0f871cb116454da40dbcf8e
Lucas <lucas.larnicol@gmail.com>
2021-07-08 14:59:33 +0200

    add IMG to logo

--- components/header/logo/logo.js
+++ components/header/logo/logo.js
@@ -1,5 +1,6 @@
 import styles from './logo.module.sass'
 import Link from 'next/link'
+import Image from 'next/image'
 
 
 
@@ -8,8 +9,10 @@ export default function Header(props){
         <Link href="https://www.llarnicol.com/" passHref>
             <article className={styles.container}>
             <figure className={styles.figure}>
-
-                <img className={styles.img} src="/images/logo/logo.svg" />
+                
+                <article>
+                    <Image src="/images/logo/logo.svg" alt='Logo Lucas Larnicol' layout="fill" />
+                </article>
 
             </figure>
             <figcaption className={styles.txt}>
