b879374ab37828670df527a0db4033d33f2fcbd1
Lucas <lucas.larnicol@gmail.com>
2021-07-08 13:34:46 +0200

    delete "'" by &apos;

--- components/info/list.js
+++ components/info/list.js
@@ -5,8 +5,8 @@ const list = [
         id: 0, 
         src: "/images/block/0.svg", 
         titre: "Lisibilité", 
-        description1: "Les données sont traités de façon la plus clair possible afin d'exploiter la mise à disposition libre d'ENEDIS",
-        description2: "Les graphiques, diagrammes circulaires et diagrammes en batons vous aidrons à trouver l'information que vous rechercher",
+        description1: "Les données sont traités de façon la plus clair possible afin d&apos;exploiter la mise à disposition libre d&apos;ENEDIS",
+        description2: "Les graphiques, diagrammes circulaires et diagrammes en batons vous aidrons à trouver l&apos;information que vous rechercher",
         color: palette[4],
         txt2Color: palette[2]
     },
@@ -14,8 +14,8 @@ const list = [
         id: 1, 
         src: "/images/block/1.svg", 
         titre: "Open Data", 
-        description1: "Les données exploité sont issus du site officiel d'ENEDIS open DATA qui permet la ré-utilisation des données numériques",
-        description2: "L'exploitation est permise grace à la license libre",
+        description1: "Les données exploité sont issus du site officiel d&apos;ENEDIS open DATA qui permet la ré-utilisation des données numériques",
+        description2: "L&apos;exploitation est permise grace à la license libre",
         color: palette[3],
         txt2Color: palette[2]
     }
