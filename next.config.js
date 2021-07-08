0fae829f464d8ec7f601f2797277ea096dae9aa9
Lucas <lucas.larnicol@gmail.com>
2021-07-08 15:52:50 +0200

    fix domains and img

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ module.exports = {
   reactStrictMode: true,
   images:{
     domains:[
-      'https://producteur.s3.eu-west-3.amazonaws.com'
+      'https://producteur.s3.eu-west-3.amazonaws.com',
+      'localhost:3000'
     ]
   }
 }
